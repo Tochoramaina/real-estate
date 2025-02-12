ca6dd6508b278a6dac7c37a7a5be5fbdbbd2c45a
Samwel Tochora <samtochora2001@gmail.com>
2025-02-12 18:13:03 +0300

    complete about page

--- client/src/Pages/About.jsx
+++ client/src/Pages/About.jsx
@@ -1,7 +1,13 @@
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="font-bold text-3xl mb-4 text-slate-800">About Maina Estate</h1>
+      <p className='mb-4 text-slate-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi ad natus in aut! Maxime quibusdam nulla numquam sapiente cum, illo necessitatibus hic laboriosam velit earum, ea quaerat dolore est minus!</p>
+      <p className='mb-4 text-slate-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit in ea magni facere eos odio impedit aliquam ducimus voluptatem enim exercitationem repellendus eveniet consectetur officia, voluptates hic eaque officiis a.</p>
+      <p className='mb-4 text-slate-600'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus vitae, cum id laboriosam delectus deleniti voluptatem quisquam saepe modi iusto ipsum mollitia similique dicta, in neque nihil tempora ex eos!</p>
+      <p className='mb-4 text-slate-600'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam, ad magni consectetur facere reprehenderit esse culpa aperiam iusto amet, repellat perferendis error ab quasi veniam optio, sint asperiores quidem id.</p>
+    </div>
   )
 }
 
